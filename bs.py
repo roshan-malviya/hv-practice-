c6cd4c534b79c45e823ad99034313e0c11f60194
roshan-malviya <roshan19@navgurukul.org>
2021-03-16 18:34:07 +0530

    bs

---
+++ bs.py
@@ -0,0 +1,20 @@
+def bs(arr,n):
+	li=0
+	ri=len(arr)-1
+	mi=0
+	while li<=ri:
+		mi=(li+ri)//2
+		mi_n=arr[mi]
+		if mi_n==n:
+			return mi
+		if mi_n< n:
+			li=mi+1
+		else:
+			ri=mi+1
+
+	return False
+
+
+a=[1,2,3,4,5,6,7]
+b=7
+print(bs(a,b))
