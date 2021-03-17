523f7e08cb7df0eba114683228511b6536222c6a
roshan-malviya <roshan19@navgurukul.org>
2021-03-17 15:47:13 +0530

    just merge two list and sorted it

--- merging_arr.py
+++ merging_arr.py
@@ -1,1 +1,9 @@
-merging_arr.py
+def merger(arr1,ar2):
+	ar1=arr1+ar2	
+	for i in range(len(ar1)):
+		for j in range((len(ar1)-1)-i):
+			if ar1[j]>ar1[j+1]:
+				ar1[j],ar1[j+1]=ar1[j+1],ar1[j]
+	return ar1
+
+print(merger([1,2,8,4],[9,5,3,7]))
